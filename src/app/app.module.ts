b82eb74b17fb90ecf345eb719c12d686f46b28b8
Brian Wilson <bwilson@mazzetti.com>
2022-07-11 08:06:27 -0500

    initial commit with angular, material and ng prime setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OrgChartComponent } from './components/org-chart/org-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrgChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
