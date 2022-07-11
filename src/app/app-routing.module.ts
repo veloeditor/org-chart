faacb34ffce710eff27a0bfe780af90acceeac25
Brian Wilson <bwilson@mazzetti.com>
2022-07-11 08:12:35 -0500

    setup routing module to route to org-chart component. added router-outlet to app-component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrgChartComponent } from './components/org-chart/org-chart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/org-chart', pathMatch: 'full' },
+  { path: 'org-chart', component: OrgChartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
